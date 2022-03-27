0d63171cbc38455dce7f214a231da1560743354e
foday <foday_s@yahoo.com>
2022-03-27 09:44:52 -0500

    useState setup
    1. created a useState by putting the count and setCount into an array and giving the count it's initial value of 4. count is the present value or state and setCount will have a funtion called decrementCount that will change that. 2. the function decrementCount() is created and in it's curly bracket setCount is written and it is (parameter) count which is the present state is decreased by 1. 3. onclick is added to the first button and it equals an object called decrementCount which is the function that sets the Count to decrease by 1

--- src/App.js
+++ src/App.js
@@ -4,12 +4,16 @@ import React, {useState} from 'react';
 function App() {
 
   const [count, setCount ] = useState(4);
+
+  function decrementCount() {
+    setCount(count - 1)
+  }
   // const onClick() {
   //   setCount() {console.log(count)}
   // }
   return (
    <> 
-      <button>-</button>
+      <button onClick={decrementCount}>-</button>
       <span>0</span>
       <button>+</button>
    </>
